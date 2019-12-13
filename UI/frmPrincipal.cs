a4c8389ba8ec7e1566acab8054581e94cd424f29
Jose Laercio <joselaerciorlima@hotmail.com>
2019-12-12 22:19:42 -0300

    Nada de mais, só pensando em  como fazer a tela principal

--- UI/frmPrincipal.cs
+++ UI/frmPrincipal.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace UI
@@ -10,33 +11,5 @@ namespace UI
          InitializeComponent();
       }
 
-        private void alunoToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-         var form = new frmCadAluno();
-         form.ShowDialog();
-        }
-
-        private void atividadeComplementarToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-         var form = new frmCadAtividade();
-         form.ShowDialog();
-      }
-
-      private void sairToolStripMenuItem_Click(object sender, EventArgs e)
-      {
-         Close();
-      }
-
-        private void cursoToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-         var form = new frmCadCurso();
-         form.ShowDialog();
-      }
-
-        private void certificadoToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-         var form = new frmCadCertificado();
-         form.ShowDialog();
-      }
-    }
+   }
 }
